85d9bfca005fa6cc859bf9472b75816e0b21a3be
zanettiprado <>
2023-05-22 18:59:26 +0000

    exercicio de random . choice str

--- run.py
+++ run.py
@@ -1,3 +1,10 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random 
+n1 = input("primeiro aluno: ")
+n2 = input("segundo aluno: ")
+n3 = input("terceiro aluno: ")
+n4 = input("quarto aluno: ")
+lista = [n1, n2, n3, n4]
+
+esc = (random.choice(lista))
+
+print(f"o escolhido foi {esc}")
